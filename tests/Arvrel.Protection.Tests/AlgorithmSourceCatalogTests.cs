626223380e250522311d3ea8d9c76f05586510bf
masarray <ari.sulistiono@gmail.com>
2026-08-01 17:32:12 +0700

    test: assert explicit feeder phasor declarations

--- tests/Arvrel.Protection.Tests/AlgorithmSourceCatalogTests.cs
+++ tests/Arvrel.Protection.Tests/AlgorithmSourceCatalogTests.cs
@@ -8,8 +8,8 @@ namespace Arvrel.Protection.Tests;
 public sealed class AlgorithmSourceCatalogTests
 {
     [DataTestMethod]
-    [DataRow("67P", "V1/I1", "DirectionalPhase67CharacteristicAngleDeg")]
-    [DataRow("67N", "3V0/3I0", "DirectionalEarth67NCharacteristicAngleDeg")]
+    [DataRow("67P", "phasor V1", "DirectionalPhase67CharacteristicAngleDeg")]
+    [DataRow("67N", "phasor polarizingVoltage = 3V0", "DirectionalEarth67NCharacteristicAngleDeg")]
     [DataRow("27", "element \"27\"", "Undervoltage27PickupV")]
     [DataRow("59", "element \"59\"", "Overvoltage59PickupV")]
     [DataRow("59N", "element \"59N\"", "ResidualOvervoltage59NPickupV")]
