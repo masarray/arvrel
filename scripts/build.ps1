fd72f24618732842097cf957f8f92fb7a1652609
masarray <ari.sulistiono@gmail.com>
2026-08-03 12:35:22 +0700

    fix: use valid PowerShell invocation syntax

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -12,17 +12,9 @@ Push-Location $root
 try {
     Stop-ArvrelDevelopmentInstances -RepositoryRoot $root
 
-    Invoke-DotNetChecked \
-        -Arguments @('restore', '.\ARVREL.sln') \
-        -FailureMessage 'ARVREL restore failed'
-
-    Invoke-DotNetChecked \
-        -Arguments @('build', '.\ARVREL.sln', '-c', $Configuration, '--no-restore') \
-        -FailureMessage 'ARVREL build failed'
-
-    Invoke-DotNetChecked \
-        -Arguments @('test', '.\ARVREL.sln', '-c', $Configuration, '--no-build') \
-        -FailureMessage 'ARVREL tests failed'
+    Invoke-DotNetChecked -Arguments @('restore', '.\ARVREL.sln') -FailureMessage 'ARVREL restore failed'
+    Invoke-DotNetChecked -Arguments @('build', '.\ARVREL.sln', '-c', $Configuration, '--no-restore') -FailureMessage 'ARVREL build failed'
+    Invoke-DotNetChecked -Arguments @('test', '.\ARVREL.sln', '-c', $Configuration, '--no-build') -FailureMessage 'ARVREL tests failed'
 }
 finally {
     Pop-Location
