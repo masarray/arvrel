1810e5e389c2214f887b6e2a52b1a70e1bc71ead
masarray <ari.sulistiono@gmail.com>
2026-08-03 13:46:05 +0700

    feat: initialize familiar overcurrent settings UX

--- src/Arvrel.App/ProtectionSettingsWindow.Initialization.cs
+++ src/Arvrel.App/ProtectionSettingsWindow.Initialization.cs
@@ -5,6 +5,7 @@ public partial class ProtectionSettingsWindow
     protected override void OnContentRendered(EventArgs e)
     {
         base.OnContentRendered(e);
+        ApplyFamiliarOvercurrentUx();
         UpdatePreviews();
     }
 }
