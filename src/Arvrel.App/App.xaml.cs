f009399d59acd4bb8d69e425c6afbe32fb946cf2
masarray <ari.sulistiono@gmail.com>
2026-08-05 19:39:44 +0700

    Qualify WPF application base type

--- src/Arvrel.App/App.xaml.cs
+++ src/Arvrel.App/App.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Threading;
 
 namespace Arvrel.App;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
